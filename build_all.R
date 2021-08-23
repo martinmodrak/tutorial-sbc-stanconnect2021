e10007e23968b5af12a565ea514e43759f5146cd
martinmodrak <modrak.mar@gmail.com>
2021-08-23 21:47:24 +0200

    Fixes to slides after first test, part 1

--- build_all.R
+++ build_all.R
@@ -1,5 +1,14 @@
-rmarkdown::render("sbc_intro.Rmd", output_dir = "./html")
-rmarkdown::render("exercise_1_theory.Rmd", output_dir = "./html")
-rmarkdown::render("exercise_2_first_SBC.Rmd", output_dir = "./html")
-rmarkdown::render("exercise_3_regression.Rmd", output_dir = "./html")
+target_dir <- "./html"
+if(!dir.exists(target_dir)) {
+  dir.create(target_dir)
+}
+rmarkdown::render("index.Rmd")
+rmarkdown::render("sbc_intro.Rmd")
+rmarkdown::render("exercise_1_theory.Rmd")
+rmarkdown::render("exercise_2_first_SBC.Rmd")
+#rmarkdown::render("exercise_2_first_SBC_solutions.Rmd")
+rmarkdown::render("exercise_3_regression.Rmd")
+
+files <- c("libs", list.files(pattern = "*.html"), list.files(pattern = "_files", include.dirs = TRUE))
+file.rename(from = files, to = paste0(target_dir, "/", files))
 
