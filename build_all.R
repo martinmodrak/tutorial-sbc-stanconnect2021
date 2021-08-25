c6c46f2dd51a2ffa83e8715cb9f208269c1e8997
martinmodrak <modrak.mar@gmail.com>
2021-08-25 09:23:14 +0200

    Modified slides for test run 2

--- build_all.R
+++ build_all.R
@@ -6,9 +6,12 @@ rmarkdown::render("index.Rmd")
 rmarkdown::render("sbc_intro.Rmd")
 rmarkdown::render("exercise_1_theory.Rmd")
 rmarkdown::render("exercise_2_first_SBC.Rmd")
-#rmarkdown::render("exercise_2_first_SBC_solutions.Rmd")
+rmarkdown::render("exercise_2_first_SBC_solutions.Rmd")
 rmarkdown::render("exercise_3_regression.Rmd")
+rmarkdown::render("conclusion.Rmd")
 
 files <- c("libs", list.files(pattern = "*.html"), list.files(pattern = "_files", include.dirs = TRUE))
 file.rename(from = files, to = paste0(target_dir, "/", files))
 
+files_copy <- "overview.png"
+file.copy(files_copy, to = paste0(target_dir, "/", files_copy))
