1eea3979f50826f5e8c78cf06f42386d1ca8058e
martinmodrak <modrak.mar@gmail.com>
2021-08-31 11:47:23 +0200

    Improving slides for the event

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Uses the `xaringan` package for slides.
 
 To compile all slides, run the `build_all.R` script, the slides
 will be stored in the `html` subfolder.
+
+
+A compiled version of the tutorial material can be found at https://www.martinmodrak.cz/post/2021-sbc_tutorial/
+
+This work was supported by ELIXIR CZ research infrastructure project (MEYS Grant No: LM2018131) including access to computing and storage facilities.
+
--- build_all.R
+++ build_all.R
@@ -4,6 +4,7 @@ if(!dir.exists(target_dir)) {
 }
 rmarkdown::render("index.Rmd")
 rmarkdown::render("sbc_intro.Rmd")
+rmarkdown::render("sbc_intro_short.Rmd")
 rmarkdown::render("exercise_1_theory.Rmd")
 rmarkdown::render("exercise_2_first_SBC.Rmd")
 rmarkdown::render("exercise_2_first_SBC_solutions.Rmd")
@@ -13,5 +14,5 @@ rmarkdown::render("conclusion.Rmd")
 files <- c("libs", list.files(pattern = "*.html"), list.files(pattern = "_files", include.dirs = TRUE))
 file.rename(from = files, to = paste0(target_dir, "/", files))
 
-files_copy <- "overview.png"
+files_copy <- list.files(pattern = "*.png")
 file.copy(files_copy, to = paste0(target_dir, "/", files_copy))
